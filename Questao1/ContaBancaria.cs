f30dcf9d65404e377702328281b6aa76bc7c17c2
Matheus Henrique Vaz Mocelin <mhmocelin@gmail.com>
2025-06-25 10:07:56 -0300

    Questao 1

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -1,9 +1,28 @@
 ﻿using System.Globalization;
 
-namespace Questao1
-{
-    class ContaBancaria {
+namespace Questao1;
 
-       
+class ContaBancaria
+{
+    private const double TaxaSaque = 3.50;
+
+    public int Numero { get; }
+    public string Titular { get; set; }
+    public double Saldo { get; private set; }
+
+    public ContaBancaria(int numero, string titular)
+    {
+        Numero = numero;
+        Titular = titular;
+        Saldo = 0.0;
     }
+
+    public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) => Deposito(depositoInicial);
+
+    public void Deposito(double valor) => Saldo += valor;
+    
+    public void Saque(double valor) => Saldo -= valor + TaxaSaque;
+
+    public override string ToString() => $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+
 }
